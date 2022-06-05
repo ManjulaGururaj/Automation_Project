eed494cb5f7097de3ae354e7911ad281fd9b5864
ManjulaGururajPatil <sinchusn.123@gmail.com>
2022-06-05 18:57:08 +0530

    multiple functions added

--- First_Project/Python_Function.py
+++ First_Project/Python_Function.py
@@ -1,13 +1,13 @@
-def add(a,b):
-    return a+b
+def add(a,b,c):
+    return a+b+c
 
 
-def sub(a,b):
-    return a-b
+def sub(a,b,c):
+    return a-b-c
 
 
-def mul(a,b):
-    return a*b
+def mul(a,b,c):
+    return a*b*c
 
 
 def div(a,b):
