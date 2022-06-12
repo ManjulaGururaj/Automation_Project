b3a4ee5266f60c1e8a15b0e44bf0c60572380bf5
ManjulaGururajPatil <sinchusn.123@gmail.com>
2022-06-12 16:49:35 +0530

    function with 3 variables

--- First_Project/Python_Function.py
+++ First_Project/Python_Function.py
@@ -2,8 +2,8 @@ def add(a,b):
     return a+b
 
 
-def sub(a,b,c):
-    return a-b-c
+def sub(a,b):
+    return a-b
 
 
 def mul(a,b,c):
@@ -14,4 +14,7 @@ def div(a,b):
     return a/b
 
 def add(a,b,c):
-    return a+b+c
+    return a+b-c
+
+def sub(a,b,c):
+    return a-b*c
